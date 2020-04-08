1574527306f8dcf4790c7539c259727d07d6ad9c
dawidpytka <44122759+dawidpytka@users.noreply.github.com>
2020-04-08 19:26:33 +0200

    dodano XLSXReader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            XLSXReader.readXLSX("pozyxAPI_dane_pomiarowe/pozyxAPI_only_localization_measurement1.xlsx");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
